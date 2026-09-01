c8bf266411ae6fa61ea68741a22524659308f485
Nairi1997 <nairi1997@users.noreply.github.com>
2026-09-01 12:02:27 +0000

    Add 45 genetics questions: p7ch1-5 (medical genetics all 5 chapters)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "eclass-quiz-v13";
+const CACHE_NAME = "eclass-quiz-v14";
 const ASSETS = ["./index.html", "./styles.css", "./app.js", "./manifest.json"];
 
 self.addEventListener("install", (e) => {
