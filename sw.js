7a0d69346e67fcbafcd756d712816314f0a432d8
Nairi1997 <nairi1997@users.noreply.github.com>
2026-09-11 12:39:18 +0000

    新增药理学第二十五章抗结核抗真菌抗病毒药18题

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "eclass-quiz-v40";
+const CACHE_NAME = "eclass-quiz-v41";
 const ASSETS = ["./index.html", "./styles.css", "./app.js", "./manifest.json"];
 
 self.addEventListener("install", (e) => {
