bdf09e6c6dcce4f6a63ba85612d4a2b7fd5c1ef9
Nairi1997 <nairi1997@users.noreply.github.com>
2026-09-11 12:12:01 +0000

    药理学第十七章组胺受体拮抗药10题（总2152题）

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "eclass-quiz-v32";
+const CACHE_NAME = "eclass-quiz-v33";
 const ASSETS = ["./index.html", "./styles.css", "./app.js", "./manifest.json"];
 
 self.addEventListener("install", (e) => {
