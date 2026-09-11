81b917285a7f70ec05974e99c130d04232e1e111
Nairi1997 <nairi1997@users.noreply.github.com>
2026-09-11 12:16:01 +0000

    药理学第十九章胰岛素及口服降血糖药18题（总2194题）

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "eclass-quiz-v34";
+const CACHE_NAME = "eclass-quiz-v35";
 const ASSETS = ["./index.html", "./styles.css", "./app.js", "./manifest.json"];
 
 self.addEventListener("install", (e) => {
