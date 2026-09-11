b60270987a26e1a33d861220a4cf4226956539c4
Nairi1997 <nairi1997@users.noreply.github.com>
2026-09-11 12:05:19 +0000

    药理学第十三章抗心律失常药18题（总2094题）

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "eclass-quiz-v28";
+const CACHE_NAME = "eclass-quiz-v29";
 const ASSETS = ["./index.html", "./styles.css", "./app.js", "./manifest.json"];
 
 self.addEventListener("install", (e) => {
